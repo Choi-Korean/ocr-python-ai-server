b145bacab350faefd0966bfcf0d622a631c51bf3
cho2_mac <chso1324@naver.com>
2022-10-11 13:52:59 +0900

    text detection 데모버젼 추가

--- controller/apps.py
+++ controller/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from text_detection.text_detection import Text_Detection
 
 from text_recognition.text_recognition import Text_Recognition
 
@@ -6,4 +7,5 @@ from text_recognition.text_recognition import Text_Recognition
 class ControllerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "controller"
+    td = Text_Detection()
     tr = Text_Recognition()
--- controller/views.py
+++ controller/views.py
@@ -43,22 +43,50 @@ class GetInformation(APIView):
             # Text Detection
             # 현재는 AI Hub Label 데이터 추출해서 Detection 하는 형식으로
             # 추출 -> static/images에 영역별로 분리해서 저장
-            with open(os.path.join(BASE_DIR, 'static', 'json', file_name + '.json'), 'r', encoding='utf8') as f:
-                json_files = json.load(f)
-            img_info = json_files['images'][0]
+            # with open(os.path.join(BASE_DIR, 'static', 'json', file_name + '.json'), 'r', encoding='utf8') as f:
+            #     json_files = json.load(f)
+            # img_info = json_files['images'][0]
 
-            # Inmemoryuploadedfile 을 이미지로 읽기
+            # # Inmemoryuploadedfile 을 이미지로 읽기
+            # img = cv2.imdecode(np.fromstring(
+            #     image.read(), np.uint8), cv2.IMREAD_COLOR)
+            # # img = cv2.imread(image, cv2.IMREAD_COLOR)
+            # targets = json_files['annotations'][0]['polygons']
+
+            # for i, l in enumerate(targets):
+            #     if l['type'] != int(2):
+            #         continue
+            #     l['points'] = sorted(l['points'], key=lambda x: (x[0], x[1]))
+            #     d1 = list(map(int, l['points'][0]))
+            #     d2 = list(map(int, l['points'][3]))
+            #     try:
+            #         sliced_img = img[d1[1]:d2[1], d1[0]:d2[0]]  # 높이, 너비
+            #         # sliced_img_name = img_info['identifier'] + \
+            #         #     f'_{i}.' + img_info['type']
+
+            #         # ndarray -> 이미지로 다시 전환해서 바로 보내기. (서버에 나눈 이미지 저장 후 불러오는게 X)
+            #         pred_img.append(Image.fromarray(sliced_img))
+            #         coordinates.append(l['points'])
+            #         # cv2.imwrite(os.path.join(BASE_DIR, 'static',
+            #         #             'images', image, sliced_img_name), sliced_img)
+            #     except:
+            #         pass
+            
+
             img = cv2.imdecode(np.fromstring(
                 image.read(), np.uint8), cv2.IMREAD_COLOR)
-            # img = cv2.imread(image, cv2.IMREAD_COLOR)
-            targets = json_files['annotations'][0]['polygons']
+            targets, _, _ = ControllerConfig.td.predict(img)
 
             for i, l in enumerate(targets):
-                if l['type'] != int(2):
-                    continue
-                l['points'] = sorted(l['points'], key=lambda x: (x[0], x[1]))
-                d1 = list(map(int, l['points'][0]))
-                d2 = list(map(int, l['points'][3]))
+                # if l['type'] != int(2):
+                #     continue
+                # l['points'] = sorted(l['points'], key=lambda x: (x[0], x[1]))
+                # d1 = list(map(int, l['points'][0]))
+                # d2 = list(map(int, l['points'][3]))
+
+                l = sorted(l, key=lambda x: (x[0], x[1]))
+                d1 = list(map(int, l[0]))
+                d2 = list(map(int, l[3]))
                 try:
                     sliced_img = img[d1[1]:d2[1], d1[0]:d2[0]]  # 높이, 너비
                     # sliced_img_name = img_info['identifier'] + \
@@ -66,7 +94,7 @@ class GetInformation(APIView):
 
                     # ndarray -> 이미지로 다시 전환해서 바로 보내기. (서버에 나눈 이미지 저장 후 불러오는게 X)
                     pred_img.append(Image.fromarray(sliced_img))
-                    coordinates.append(l['points'])
+                    coordinates.append(l)
                     # cv2.imwrite(os.path.join(BASE_DIR, 'static',
                     #             'images', image, sliced_img_name), sliced_img)
                 except:
@@ -80,28 +108,33 @@ class GetInformation(APIView):
 
             res = Responsed(user=user, result=result)
 
-            for i in range(len(coordinates)):
-                points = np.array([list(map(int, p))
-                                  for p in coordinates[i]]).astype(np.int32)
-                txt = result[0]['result'][0][i]
-                center = [(points[3][0] + points[0][0]) // 2,
-                          (points[3][1] + points[0][1]) // 2]
-                m = points[3][0] - center[0]
-                cv2.rectangle(img, points[0], points[3],
-                              (255, 0, 255), 2, cv2.LINE_AA)
-                b, g, r, a = 100, 100, 250, 0
-                fontpath = "fonts/gulim.ttc"
-                font = ImageFont.truetype(fontpath, 60)
-                img_pil = Image.fromarray(img)
-                draw = ImageDraw.Draw(img_pil)
-                draw.text((center[0] + m * 2, center[1] - m),
-                          txt, font=font, fill=(b, g, r, a))
-                img = np.array(img_pil)
-
-            img = cv2.resize(img, (800, 1000))
-            cv2.imshow('img', img)  # [:, :, ::-1])
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
 
             return Response(ResponsedSerializer(res).data, status=status.HTTP_200_OK)
         return Response({'Bad Request': 'Invalid Data..'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        # print(ControllerConfig.td.predict(img))
+
+
+            # for i in range(len(coordinates)):
+            #     points = np.array([list(map(int, p))
+            #                       for p in coordinates[i]]).astype(np.int32)
+            #     txt = result[0]['result'][0][i]
+            #     center = [(points[3][0] + points[0][0]) // 2,
+            #               (points[3][1] + points[0][1]) // 2]
+            #     m = points[3][0] - center[0]
+            #     cv2.rectangle(img, points[0], points[3],
+            #                   (255, 0, 255), 2, cv2.LINE_AA)
+            #     b, g, r, a = 100, 100, 250, 0
+            #     fontpath = "fonts/gulim.ttc"
+            #     font = ImageFont.truetype(fontpath, 60)
+            #     img_pil = Image.fromarray(img)
+            #     draw = ImageDraw.Draw(img_pil)
+            #     draw.text((center[0] + m * 2, center[1] - m),
+            #               txt, font=font, fill=(b, g, r, a))
+            #     img = np.array(img_pil)
+
+            # img = cv2.resize(img, (800, 1000))
+            # cv2.imshow('img', img)  # [:, :, ::-1])
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
